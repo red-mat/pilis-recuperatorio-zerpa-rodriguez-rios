558ed585b632538a3a65b6bccb6a62bcfb298b8c
red-mat <jesus.ing.rodriguez@gmail.com>
2023-02-09 15:40:33 -0300

    test(user-management): complete test

--- src/test/uses/storage.test.js
+++ src/test/uses/storage.test.js
@@ -1,18 +1,68 @@
 import { Storage } from '@/uses/storage'
+import { expect, test } from 'vitest'
 
 const key = 'test'
-const def = ''
+const def = 'hola mundo'
 
 describe('Storage uses', () => {
-  const STest = Storage(key, def)
+  const storage = Storage(key, def)
 
   test('should init storage', () => {
     let item = localStorage.getItem(key)
-
     expect(item).toBeNull()
-    STest.init()
+
+    storage.init()
 
     item = localStorage.getItem(key)
     expect(item).toEqual(def)
+  })
+
+  test('should delete storage', () => {
+    storage.del()
+    const item = localStorage.getItem(key)
+    expect(item).toBeNull()
+  })
+
+  test('should existence storage', () => {
+    storage.create()
+    expect(storage.exist()).toBeTruthy()
+
+    storage.del()
+    expect(storage.exist()).toBeFalsy()
+  })
+
+  test('should get storage', () => {
+    const item = localStorage.getItem(key)
+    expect(storage.get()).toEqual(item)
+  })
+
+  test('should set storage', () => {
+    const value = 'adios mundo'
+    storage.set(value)
+
+    const item = localStorage.getItem(key)
+    expect(storage.get()).toEqual(item)
+  })
+
+  test('should restore storage', () => {
+    const value = 'chanchito feliz'
+
+    storage.init()
+    expect(storage.get()).toEqual(def)
+
+    storage.set(value)
+    expect(storage.get()).toEqual(value)
+
+    storage.init()
+    expect(storage.get()).toEqual(def)
+  })
+
+  test('should not recreate storage ', () => {
+    const value = 'wazaaaaa'
+
+    storage.set(value)
+    storage.create()
+
+    expect(storage.get()).toEqual(value)
   })
 })
