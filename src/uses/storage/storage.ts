93518c2723c1f541e69ed58c48bfb497ba0809fe
red-mat <jesus.ing.rodriguez@gmail.com>
2023-02-09 00:26:41 -0300

    refactor(uses): storage swap methods init and create

--- src/uses/storage/storage.ts
+++ src/uses/storage/storage.ts
@@ -2,9 +2,9 @@ export function Storage(key: string, def: string) {
   const get = () => localStorage.getItem(key)
   const set = (data: string) => localStorage.setItem(key, data)
   const del = () => localStorage.removeItem(key)
-  const create = () => localStorage.setItem(key, def)
+  const init = () => localStorage.setItem(key, def)
   const exist = () => get() !== null
-  const init = () => exist() || create()
+  const create = () => exist() || init()
 
   return {
     get,
