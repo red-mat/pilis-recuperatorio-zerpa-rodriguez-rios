fa5c646d3b31af31fccf41bf604642d04c4a181a
red-mat <jesus.ing.rodriguez@gmail.com>
2023-02-13 18:15:21 -0300

    feat(test): finaly test api

--- src/test/services/trivia.test.js
+++ src/test/services/trivia.test.js
@@ -1,5 +1,6 @@
-import { getCategory, getTags } from '@/services/trivia'
-import { expect } from 'vitest'
+import { getCategory, getTags, TriviaApi } from '@/services/trivia'
+import { expect, test } from 'vitest'
+import regions from '@/data/regions.json'
 
 describe('Trivia Api', () => {
   let tags
@@ -14,5 +15,23 @@ describe('Trivia Api', () => {
     category = await getCategory()
 
     expect(category['Music']).toBeDefined()
+  })
+
+  test('get questions from regions', async () => {
+    const locations = Object.keys(regions)
+    const preferences = { limit: 1, region: regions[locations[130]] }
+
+    const quiz = await TriviaApi.getQuestions(preferences)
+
+    expect(quiz).toHaveLength(1)
+  })
+
+  test('get questions from tags', async () => {
+    const testTags = [tags[3], tags[1], tags[2]]
+    const preferences = { limit: 1, tags: testTags }
+
+    const quiz = await TriviaApi.getQuestions(preferences)
+
+    expect(quiz).toHaveLength(1)
   })
 })
