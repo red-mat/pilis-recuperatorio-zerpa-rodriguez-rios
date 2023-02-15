d11e2a1d5a1a32c1815940d6b34c1910572056f7
Micaela Zerpa <micazerpa14@gmail.com>
2023-02-15 10:54:02 -0300

    refactor(components): update form trivia

--- src/components/FormTrivia/FormTrivia.jsx
+++ src/components/FormTrivia/FormTrivia.jsx
@@ -4,49 +4,34 @@ import SelectTags from './components/SelectTags'
 import SelectRegion from './components/SelectRegion'
 import { useForm } from 'react-hook-form'
 import SelectDifficultty from './components/SelectDifficultty'
-import { useState } from 'react'
+import SelectLimit from './components/SelectLimit'
 
 const FormTrivia = ({ onSubmit }) => {
   const hookForm = useForm()
-  const [num, setNum] = useState(0)
 
   return (
-    <div>
-      <form onSubmit={hookForm.handleSubmit(onSubmit)}>
-        <h2>..............The trivia..............</h2>
-        <div className="form__input">
-          <SelectCategory className="form__select" hookForm={hookForm} />
-        </div>
-        <div className="form__input">
-          <SelectDifficultty className="form__select" hookForm={hookForm} />
-        </div>
-        <div className="form__input">
-          <input
-            className="input__range"
-            min="0"
-            max="20"
-            type="range"
-            value={num}
-            onChange={e => setNum(e.target.value)}
-            hookForm={hookForm}
-          />
-          <h3>{num}</h3>
-        </div>
-        <div className="form__input">
-          <SelectTags
-            className="form__select form__select--tags"
-            hookForm={hookForm}
-          />
-        </div>
-        <div className="form__input">
-          <SelectRegion className="form__select" hookForm={hookForm} />
-        </div>
+    <>
+      <form className="form" onSubmit={hookForm.handleSubmit(onSubmit)}>
+        <h2>The trivia</h2>
+
+        <SelectCategory className="form__select" hookForm={hookForm} />
+
+        <SelectDifficultty className="form__select" hookForm={hookForm} />
+
+        <SelectLimit
+          className="form__select form__select--range"
+          hookForm={hookForm}
+        />
+
+        <SelectTags className="form__select" hookForm={hookForm} />
+
+        <SelectRegion className="form__select" hookForm={hookForm} />
 
         <button className="form__btn" type="submit">
           start
         </button>
       </form>
-    </div>
+    </>
   )
 }
 export default FormTrivia
