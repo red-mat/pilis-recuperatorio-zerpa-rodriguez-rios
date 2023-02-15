556b62b4afa7ad7a2a7ca1741655421a189c993b
red-mat <jesus.ing.rodriguez@gmail.com>
2023-02-15 17:37:42 -0300

    feat(services): Quiz class resolve question from response trivia api

--- src/test/services/trivia.test.js
+++ src/test/services/trivia.test.js
@@ -1,6 +1,7 @@
 import { getCategory, getTags, TriviaApi } from '@/services/trivia'
-import { expect, test } from 'vitest'
+import { describe, expect, test } from 'vitest'
 import regions from '@/data/regions.json'
+import { Quiz } from '@/services/trivia/quiz'
 
 describe('Trivia Api', () => {
   let tags
@@ -33,5 +34,60 @@ describe('Trivia Api', () => {
     const quiz = await TriviaApi.getQuestions(preferences)
 
     expect(quiz).toHaveLength(1)
+  })
+})
+
+describe('Trivia QUIZ', () => {
+  let QuizID = {}
+  let questions = []
+
+  test('should Quiz', async () => {
+    questions = await TriviaApi.getQuestions({ limit: 5 })
+
+    for (const question of questions) {
+      QuizID[question.id] = new Quiz(question)
+    }
+
+    for (const question of questions) {
+      expect(QuizID[question.id].getData().id).toEqual(question.id)
+    }
+  })
+
+  test('should getAnswers', () => {
+    // compare two arrays
+    const isEqual = (a = [], b = []) => {
+      if (a.pop() !== b.pop()) return false
+      if (a.length === 0) return true
+      isEqual(a, b)
+    }
+
+    // get answers from question object
+    const getAnswers = question => [
+      ...question.incorrectAnswers,
+      question.correctAnswer,
+    ]
+
+    // compare if answersQuiz and answers questions are not equals
+    for (const question of questions) {
+      const answersQuiz = QuizID[question.id].getAnswers()
+      const answers = getAnswers(question)
+
+      expect(answers.length === answersQuiz.length).toBeTruthy()
+      expect(isEqual(answers, answersQuiz)).toBeFalsy()
+    }
+  })
+
+  test('should get resolve state', () => {
+    for (const question of questions) {
+      const id = question.id
+
+      // return true with correct answers
+      let isResolve = QuizID[id].resolve(question.correctAnswer)
+      expect(isResolve).toBeTruthy()
+
+      // return false with incorrect answers
+      isResolve = QuizID[id].resolve(question.question)
+      expect(isResolve).toBeFalsy()
+    }
   })
 })
