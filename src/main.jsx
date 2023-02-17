f78f437d681f1a5234abb67daecbb62ea75a99e4
red-mat <jesus.ing.rodriguez@gmail.com>
2023-02-17 19:46:47 -0300

    feat(main): implement QuieryClientProvider

--- src/main.jsx
+++ src/main.jsx
@@ -5,15 +5,20 @@ import '@/css/index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { LoginProvider } from './context/login/provider'
 import { PreferencesProvider } from './context/preferences'
+import { QueryClient, QueryClientProvider } from 'react-query'
+
+const client = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <LoginProvider>
-        <PreferencesProvider>
-          <App />
-        </PreferencesProvider>
-      </LoginProvider>
+      <QueryClientProvider client={client}>
+        <LoginProvider>
+          <PreferencesProvider>
+            <App />
+          </PreferencesProvider>
+        </LoginProvider>
+      </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>
 )
