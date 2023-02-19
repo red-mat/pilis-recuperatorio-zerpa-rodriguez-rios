4d90f014022a4220c0bb5f209818a35ee5121a68
red-mat <jesus.ing.rodriguez@gmail.com>
2023-02-19 20:50:48 -0300

    refactor(componets): implement hook form in Trivia

--- src/components/Trivia/Trivia.jsx
+++ src/components/Trivia/Trivia.jsx
@@ -1,33 +1,44 @@
 import './Trivias.css'
 
 import { usePreferencesContext } from '@/context/preferences'
-import CardTrivia from './components/CardTrivia'
 import { useTrivia } from '@/hooks/trivia/useTrivia'
+import { useForm, FormProvider } from 'react-hook-form'
+
+import CardTrivia from './components/CardTrivia'
 
 const ListQuiz = ({ trivia }) => {
   return (
     <div className="trivias">
-      {trivia.getQuiz().map(q => (
-        <CardTrivia key={q.getData().id} data={q.getData()} />
-      ))}
+      {trivia.getQuiz().map(q => {
+        const id = q.getData().id
+        const question = q.getQuestion()
+        const answers = q.getAnswers()
+
+        return (
+          <CardTrivia key={id} id={id} question={question} answers={answers} />
+        )
+      })}
     </div>
   )
 }
 
-export const Trivia = ({ onFinish }) => {
+export const Trivia = ({ onSubmit, trivia }) => {
   const { preferences } = usePreferencesContext()
   const { trivia, loading } = useTrivia(preferences)
-
-  const onSubmit = data => console.log(data)
+  const hookForm = useForm()
 
   if (loading) return <h2>loading</h2>
   return (
     <>
+      {/* TODO: remove title */}
       <h2>The trivias</h2>
-      <form>
-        <ListQuiz trivia={trivia} />
-        <button onClick={onSubmit}>finalizar</button>
-      </form>
+
+      <FormProvider {...hookForm}>
+        <form onSubmit={onSubmit}>
+          <ListQuiz trivia={trivia} />
+          <button>finalizar</button>
+        </form>
+      </FormProvider>
     </>
   )
 }
--- src/components/Trivia/components/CardTrivia.jsx
+++ src/components/Trivia/components/CardTrivia.jsx
@@ -1,45 +1,23 @@
-import { useEffect, useState } from 'react'
 import './CardTrivia.css'
 
-const exampleData = {
-  category: 'Sport & Leisure',
-  id: '62417cbc8c3137686862d735',
-  correctAnswer: 'Atlanta Falcons',
-  incorrectAnswers: ['Atlanta Dynamo', 'Atlanta Marlins', 'Atlanta Rockets'],
-  question: 'Which of these is an American Football team based in Atlanta?',
-  tags: ['nfl', 'american_football', 'usa', 'sport'],
-  type: 'Multiple Choice',
-  difficulty: 'hard',
-  regions: [],
-  isNiche: false,
-}
+import { useFormContext } from 'react-hook-form'
 
-const CardTrivia = ({ data = exampleData }) => {
-  const [respuestas, setRespuestas] = useState([])
-
-  useEffect(() => {
-    const newRespuestas = [...data.incorrectAnswers]
-    newRespuestas.push(data.correctAnswer)
-
-    setRespuestas(newRespuestas)
-    //console.log(newRespuestas)
-  }, [])
-
+const CardTrivia = ({ id, question, answers }) => {
+  const { register } = useFormContext()
   return (
     <>
       <fieldset className="fieldset__block">
-        <legend className="block__question">{data.question}</legend>
+        <legend className="block__question">{question}</legend>
 
-        {respuestas.map(respuesta => (
-          <label htmlFor={respuesta} key={respuesta} className="block__answer">
+        {answers.map(answer => (
+          <label key={answer} className="block__answer">
             <input
-              className="radio__botton"
-              id={data.id}
-              name={data.id}
+              {...register(id)}
+              className="radio__bottom"
               type="radio"
-              value={respuesta}
+              value={answer}
             />
-            {respuesta}
+            {answer}
           </label>
         ))}
       </fieldset>
