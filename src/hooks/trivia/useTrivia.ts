6cda6ed647c4b4bcb85d36f00ed724b4b60a7f5e
red-mat <jesus.ing.rodriguez@gmail.com>
2023-02-20 23:24:19 -0300

    refactor(hooks): delete useQuery to useTrivia

--- src/hooks/trivia/useTrivia.ts
+++ src/hooks/trivia/useTrivia.ts
@@ -1,24 +1,25 @@
 import { Trivia, TriviaApi } from '@/services/trivia'
 import { IPreferences } from '@/types/trivia'
-import { useEffect, useState } from 'react'
-import { useQuery } from 'react-query'
+import { useCallback, useEffect, useState } from 'react'
 
 export function useTrivia(preferences: IPreferences) {
   const [trivia, setTrivia] = useState<Trivia>(undefined)
-  const [loading, setLoading] = useState(true)
-  const { data, isLoading, refetch } = useQuery('questions', () =>
+  const [isLoading, setIsLoading] = useState(true)
+
+  const fetchTrivia = useCallback(() => {
+    setIsLoading(true)
     TriviaApi.getQuestions(preferences)
-  )
+      .then(q => setTrivia(new Trivia(q)))
+      .then(() => setIsLoading(false))
+  }, [preferences])
 
   useEffect(() => {
-    if (!isLoading) {
-      setTrivia(() => new Trivia(data))
-    }
-  }, [isLoading])
-  useEffect(() => {
-    if (trivia === undefined) setLoading(true)
-    else setLoading(false)
-  }, [trivia])
+    fetchTrivia()
+  }, [fetchTrivia])
 
-  return { loading, trivia, refetch }
+  const refetch = useCallback(() => {
+    fetchTrivia()
+  }, [fetchTrivia])
+
+  return { isLoading, trivia, refetch }
 }
