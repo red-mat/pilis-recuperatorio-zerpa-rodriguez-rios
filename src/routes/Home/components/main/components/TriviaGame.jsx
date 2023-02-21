5a93ab16d8506d4aceaacb123cd8379430685451
red-mat <jesus.ing.rodriguez@gmail.com>
2023-02-21 00:07:53 -0300

    feat: finish trivia services

--- src/routes/Home/components/main/components/TriviaGame.jsx
+++ src/routes/Home/components/main/components/TriviaGame.jsx
@@ -1,18 +1,27 @@
-import { Trivia } from '@/components'
+import { Trivia as FormTrivia } from '@/components'
 import { CondComp } from '@/components/CondComp/CondComp'
 import { useTrivia } from '@/hooks/trivia/useTrivia'
+import { useMainContext } from '../context/context'
+import { useHandleTrivia } from '../hooks/useHandleTrivia'
 
 export function TriviaGame({ preferences }) {
-  const { loading, trivia } = useTrivia(preferences)
+  const { isLoading, trivia, refetch } = useTrivia(preferences)
+  const onSubmit = useHandleTrivia(trivia)
+  const {
+    finish: { isFinish },
+    points: { points },
+  } = useMainContext()
 
   return (
-    <CondComp conditional={loading}>
+    <CondComp conditional={isLoading}>
       <CondComp.WhenTrue>
         <h2>Loading...</h2>
       </CondComp.WhenTrue>
 
       <CondComp.WhenFalse>
-        <Trivia onSubmit={data => console.log(data)} trivia={trivia} />
+        <FormTrivia onSubmit={onSubmit} trivia={trivia} />
+
+        {isFinish && <p>{points}</p>}
       </CondComp.WhenFalse>
     </CondComp>
   )
