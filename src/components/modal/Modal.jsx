4a2e02fb93db69d46a24d7a793c8c18d7589d65f
red-mat <jesus.ing.rodriguez@gmail.com>
2023-02-21 00:16:22 -0300

    feat(components): make modal component

---
+++ src/components/modal/Modal.jsx
@@ -0,0 +1,24 @@
+import { useEffect } from 'react'
+import { createPortal } from 'react-dom'
+
+export default function ViewModal({ isOpen, handleClose, children }) {
+  const content = document.createElement('div')
+
+  useEffect(() => {
+    document.body.appendChild(content)
+    return () => {
+      content.remove()
+    }
+  }, [content])
+
+  if (isOpen)
+    return createPortal(
+      <div className="portal">
+        <div className="portal__close" onClick={handleClose}>
+          x
+        </div>
+        {children}
+      </div>,
+      content
+    )
+}
