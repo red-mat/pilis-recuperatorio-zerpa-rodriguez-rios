9025f814295458cce257616c52bf10314eed9de9
Micaela Zerpa <micazerpa14@gmail.com>
2023-02-22 11:54:16 -0300

    feat: remove react-query

--- src/main.jsx
+++ src/main.jsx
@@ -4,18 +4,13 @@ import App from './App'
 import '@/css/index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { LoginProvider } from './context/login/provider'
-import { QueryClient, QueryClientProvider } from 'react-query'
-
-const client = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <QueryClientProvider client={client}>
-        <LoginProvider>
-          <App />
-        </LoginProvider>
-      </QueryClientProvider>
+      <LoginProvider>
+        <App />
+      </LoginProvider>
     </BrowserRouter>
   </React.StrictMode>
 )
